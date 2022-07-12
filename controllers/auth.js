30a84069f7bd9735bbab838be208b22cf0191cf4
Justin Bourgeois <99760766+BourgeoisBoujjie@users.noreply.github.com>
2022-07-13 00:22:04 +0700

    built int controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,11 +4,47 @@ const res = require('express/lib/response')
 const router = express.Router()
 
 
-// controller
+// controller GET
 router.get('/', (req, res) =>{
-    // res.render('auth')
-    res.send('hello from auth')
+    res.render('auth')
+    // res.send('hello from auth')
 })
+
+router.get('/login', (req, res) => {
+    res.render('login')
+})
+
+router.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
+router.get('/logout', (req, res) => {
+    res.render('houses')
+})
+
+//  controller POST
+router.post('/login', (req, res) => {
+    res.render('login')
+})
+
+router.post('/signup', async (req, res) => {
+    try{
+        console.log(req.body)
+        let foundUser = await URLSearchParams.findOne({
+            email: req.body.email,
+            if(foundUser){
+                throw new Error('This is not the user we are looking for Trooper')
+            },
+            
+        })
+        let user = await Usersers.create(req.body)
+        req.login(req.body.user)
+    }catch{
+        throw err
+    }
+    res.redirect('/houses')
+})
+
 
 // export
 module.exports = router
--- controllers/bookings.js
+++ controllers/bookings.js
@@ -9,6 +9,9 @@ router.get('/', (req, res) =>{
     // res.render('bookings')
     res.send('hello from bookings')
 })
+router.post('/bookings', (req, res) => {
+    res.render('/bookings')
+})
 
 // export
 module.exports = router
