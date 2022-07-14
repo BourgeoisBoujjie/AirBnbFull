61e269208a93e874d5247da15a538a5b1fb593b8
Justin Bourgeois <99760766+BourgeoisBoujjie@users.noreply.github.com>
2022-07-14 14:37:09 +0700

    forgot to commit after posting new doc to db, and redirect

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
 // imported packages
 const express = require('express')
+const { json } = require('express/lib/response')
 const res = require('express/lib/response')
 const router = express.Router()
+const Users = require('../models/users')
 
 
 // controller GET
@@ -27,22 +29,23 @@ router.post('/login', (req, res) => {
     res.render('login')
 })
 
-router.post('/signup', async (req, res) => {
+router.post('/signup', async (req, res, next) => {
     try{
         console.log(req.body)
-        let foundUser = await URLSearchParams.findOne({
+        let foundUser = await Users.findOne({
             email: req.body.email,
             if(foundUser){
-                throw new Error('This is not the user we are looking for Trooper')
-            },
-            
+                throw new Error('This is not the user we are looking for')
+            },            
         })
-        let user = await Usersers.create(req.body)
-        req.login(req.body.user)
-    }catch{
-        throw err
+        let user = await Users.create(req.body)
+        req.login(user, (err) =>{
+            if(err){throw err}
+        })
+        res.redirect('/houses')
+    }catch (err){
+        next (err)
     }
-    res.redirect('houses/list')
 })
 
 
